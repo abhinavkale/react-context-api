298638d12c11b4bb4f22ccdd28894bf8d2ce6c5d
Abhinav Kale <56266493+abhinavkale@users.noreply.github.com>
2021-04-05 23:11:04 +0530

    react-context-api

--- src/App.js
+++ src/App.js
@@ -1,26 +1,54 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { Fragment } from "react"
 
-function App() {
+import Provider from "./provider"
+import Context from "./context"
+
+
+const Agents = () => {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <AgentOne />
+  )
+}
+
+const AgentOne = () => {
+  return (
+    <AgentTwo />
+  )
+}
+
+const AgentTwo = () => {
+  return (
+    <AgentBond />
+  )
+}
+
+const AgentBond = () => {
+  return (
+    <Context.Consumer>
+      {
+        (context) => (
+          <Fragment>
+            <h3>Agent Information</h3>
+            <p>Mission Name: {context.data.mname}</p>
+            <h2>Mission Status: {context.data.accept}</h2>
+            <button onClick={context.isMissionAccepted}>Choose to accept</button>
+          </Fragment>
+        )
+      }
+    </Context.Consumer>
+  )
+}
+
+const App = () => {
+  return (
+    <div>
+      <h1>Context API</h1>
+      <Provider>
+        <Agents />
+      </Provider>
     </div>
-  );
+  )
 }
 
 export default App;
+
